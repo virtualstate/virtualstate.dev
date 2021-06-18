cd1200cb227bbc8258e8a63ac84b7cc7bfc38148
Fabian Cook <hello@fabiancook.dev>
2021-06-18 23:54:33 +1200

    Add logo

--- src/contents/intro.tsx
+++ src/contents/intro.tsx
@@ -6,7 +6,7 @@ import { Template } from '../template';
 export const Intro = (
   <>
     <Template id="intro-template">
-      <h1>Virtual State X</h1>
+      <h1>Virtual State <span class="branded-x">X</span></h1>
       <div class="quote">
         Psst... see the <a href="https://github.com/virtualstate/x" target="_blank" rel="noopener">GitHub repository for this project here</a>
         &nbsp;or alternatively <a href="https://www.npmjs.com/package/@virtualstate/x" target="_blank" rel="noopener">the npm package here</a>
